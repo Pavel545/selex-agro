67f8edd784dc41ea91767ee7b9914cf1720b5a21
Pavel545 <p_levin00@mail.ru>
2025-10-16 23:29:42 +0400

    16.10.25

--- src/pages/ourProducts/ourProducts.jsx
+++ src/pages/ourProducts/ourProducts.jsx
@@ -10,15 +10,15 @@ export default function OurProducts(params) {
         <Banner
             img={"/img/OurProducts.jpg"}
             title={"НАША ПРОДУКЦИЯ"}
-            description={"Решает главные задачи АПК: от здоровья растений и <br> урожайности до благополучия животных и кормовой<br> безопасности."}
+            description={"Решает главные задачи АПК: от здоровья растений и <br class='br-desk'> урожайности до благополучия животных и кормовой<br class='br-desk'> безопасности."}
             textBut={"Перейти к товарам"}
             link={"#goods"}
 
-                seoTitle="Продукция СИЛЕКС-АГРО | Биопрепараты для АПК"
-    seoDescription="Биофунгициды, биоинсектициды, биостимуляторы роста, пробиотики для животных. Технология SILEX® с диоксидом кремния."
-    seoKeywords="биофунгициды, биоинсектициды, биостимуляторы, пробиотики, сорбенты микотоксинов"
-    ogImage="/img/OurProducts.jpg"
-    canonical="https://silex-agro.ru/our_products"
+            seoTitle="Продукция СИЛЕКС-АГРО | Биопрепараты для АПК"
+            seoDescription="Биофунгициды, биоинсектициды, биостимуляторы роста, пробиотики для животных. Технология SILEX® с диоксидом кремния."
+            seoKeywords="биофунгициды, биоинсектициды, биостимуляторы, пробиотики, сорбенты микотоксинов"
+            ogImage="/img/OurProducts.jpg"
+            canonical="https://silex-agro.ru/our_products"
         />
         <Goods all />
         <Fos img={"/img/fonFOS2.jpg"} />
